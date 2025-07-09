899579a9129342325712fc2412b9090bab89a911
LEE-GYEO-RE <rufp9891@gmail.com>
2025-07-10 00:37:50 +0900

    day 08 심화과제1 완료 질문 몇개 있음

--- src/심화/심화과제1/BankController1.java
+++ src/심화/심화과제1/BankController1.java
@@ -2,18 +2,82 @@ package 심화.심화과제1;
 
 public class BankController1 {
 
+
+
     // 클래스 3개 생성
     // 객체 100개 저장할 수 있는 배열 생성
-    // 매개변수로 입력받을 값 지정
+    // 매개변수로 입력받을 값 지정 -> 생성자 생성 시 같이 할 예정
     // 메소드 생성
-        // 1. 계좌등록 메소드
-        // 2. 입금 메소드
-            // 1) 입금 성공 시 출력 기능
-            // 2) 계좌번호 다를 때 , 입금 실패 출력 기능
-        // 3. 출금 메소드
-            // 1) 출금 성공 시 출력 가능
-            // 2) 출금 잔액 부족 시 , 출금 실패 출력 기능
-        // 4. 잔고 출력 메소드
+    // 1. 계좌등록 메소드
+    // 2. 입금 메소드
+        // 1) 입금 성공 시 출력 기능
+        // 2) 계좌번호 다를 때 , 입금 실패 출력 기능
+    // 3. 출금 메소드
+        // 1) 출금 성공 시 출력 가능
+        // 2) 출금 잔액 부족 시 , 출금 실패 출력 기능
+    // 4. 잔고 출력 메소드
     // 메소드 호출
     // 출력
-}
+
+    // 풀면서 질문 :
+    // 1. 생성자의 역할 정확히 알아야 함, 객체 생성해서 값을 넣어 저장해야하는데 , 어떤 속성값의 객체인지 지정해줘야함
+    // 2. return -1 음수로 오류 표현? 이게 뭔지 모르겠음
+
+
+    Account1[] accounts = new Account1[100];                   // 객체 100개 저장할 수 있는 accounts
+
+    // 계좌 등록 메소드
+    boolean getAccount(String account, int password) {
+        // boolean 타입의 getAccount 메소드 생성 , String  account , int password를 매개변수로 지정
+        Account1 account1 = new Account1(account , password);  // account1 객체 생성 -> Account1 생성자에서 값 가져옴
+        for (int i = 0; i <= accounts.length - 1; i++) {    // accounts 배열의 i번째까지 순회
+            if (accounts[i] == null) {                     // accounts[i] 번째가 null 이면
+                accounts[i] = account1;                    // accounts[i] 번째에 account1 대입
+                return true;                                // true 반환 , 반복문 종료
+            } // if end
+        } // for end
+        return false;                                       // null이 존재 하지 않으면 false
+    } // func end
+
+
+    // 입금 메소드
+    boolean getDeposit( String account , int password , int deposit ){
+        for( int i = 0; i<= accounts.length -1 ; i++){
+            if(accounts[i] != null // null 이지 않으면서
+                    && accounts[i].account.equals(account) && accounts[i].password == password){
+                // accounts[i] 번째 인덱스의 계좌번호랑 , 입력받은 계좌번호 같으면서
+                // accounts[i] 번째 인덱스의 비밀번호랑 , 입력받은 비밀번호도 같으면
+                accounts[i].money += deposit;
+                // accounts[i] 번째 인덱스의 잔액에 입금액 누적 더하기
+                return true;    //  반복문 종료
+            } // if end
+        } // for end
+        return false;   // 하나라도 틀리면 경고
+    } // func end
+
+    // 출금 메소드
+    boolean getWithdrawal( String account , int password ,int withdrawal ){
+        for( int i = 0 ; i <=accounts.length -1 ; i++){
+            if( accounts[i] == null ) continue;     // null 이면 계속 찾음
+            if( accounts[i].account.equals(account) && accounts[i].password == password ){  // 계좌랑 비번 같으면
+                if( accounts[i].money >= withdrawal ) { // 돈이 출금액보다 적거나 같으면
+                    accounts[i].money -= withdrawal;    // 돈에 출금액 뺀 금액 대입
+                    return true;                        // true 반환 메소드 종료
+                }else { return false; }
+            } // if end
+        } // for end
+        return false;                                   // 계좌 못찾음
+    } // func end
+
+    // 잔고 출력 메소드
+    int getMoney( String account , int password ){
+        for( int i = 0 ; i <=accounts.length -1 ; i++){
+            if( accounts[i] == null ) continue;
+            if(accounts[i].account.equals(account) && accounts[i].password == password){
+                return accounts[i].money;
+            } // if end
+        } // for end
+        return -1; // 음수로 오류 표현? 일단 ㅇㅋ
+    } // func end
+
+} // class end
--- src/심화/심화과제1/BankService1.java
+++ src/심화/심화과제1/BankService1.java
@@ -5,13 +5,16 @@ import java.util.Scanner;
 public class BankService1 {
     public static void main(String[] args) {
 
+        BankController1 bc = new BankController1();
+        // BankController1 클래스에 있는 메소드들 호출하기 위해서 bc 객체 생성
+        Scanner scan = new Scanner(System.in);
+        // 무한 루프 밖에 지정하는 게 메모리에 좋음
         for( ; ; ){
             System.out.println("============== KB Bank  ==============");
             System.out.println("1.계좌등록 | 2.입금 | 3.출금 | 4.잔고");
             System.out.println("======================================");
             System.out.println("선택 > ");
 
-            Scanner scan = new Scanner(System.in);
             int choose = scan.nextInt();
 
             if( choose == 1 ){
@@ -19,8 +22,15 @@ public class BankService1 {
                 System.out.println("--- 계좌 등록 ---");
                 System.out.println("계좌번호 : ");  String account = scan.next();
                 System.out.println("비밀번호 : ");  int password = scan.nextInt();
-                System.out.println("[안내] 계좌 등록이 완료되었습니다.");
-            } // if e
+
+                boolean result = bc.getAccount( account , password );
+                // 입력받은 값을 인수로 전달 후 , getAccount 메소드 실행한 결과를 result 변수에 저장 ,
+                if( result ) { // 메소드 실행 후 , return true 이면 아래 출력
+                    System.out.println("[안내] 계좌 등록이 완료되었습니다.");
+                }else { // 메소드 실행 후 null 존재하지 않으면 , 아래출력
+                    System.out.println("[경고] 등록 가능 계좌의 한도가 넘었습니다.");
+                }
+            } // if1 e
 
             if( choose == 2 ){
                 System.out.println("============== KB Bank  ==============");
@@ -28,27 +38,39 @@ public class BankService1 {
                 System.out.println("계좌번호 :");   String account = scan.next();
                 System.out.println("비밀번호 :");   int password = scan.nextInt();
                 System.out.println("입금액 :");    int deposit = scan.nextInt();
-                System.out.println("[안내] 입금이 완료되었습니다.");
-                System.out.println("[경고] 계좌정보가 일치하지 않습니다.");
-            } // if e
+                boolean result1 = bc.getDeposit(account ,password ,deposit);
+                if( result1 ) {
+                    System.out.println("[안내] 입금이 완료되었습니다.");
+                }else {
+                    System.out.println("[경고] 계좌정보가 일치하지 않습니다.");
+                }
+            } // if2 e
 
             if( choose == 3 ){
                 System.out.println("============== KB Bank  ==============");
                 System.out.println("--- 출금 ---");
                 System.out.println("계좌번호 :");   String account = scan.next();
                 System.out.println("비밀번호 :");   int password = scan.nextInt();
-                System.out.println("출금액 :");    int Withdrawal = scan.nextInt();
-                System.out.println("[안내] 출금이 완료되었습니다.");
-                System.out.println("[경고] 잔액이 부족합니다.");
-            } // if e
+                System.out.println("출금액 :");    int withdrawal = scan.nextInt();
+                boolean result2 = bc.getWithdrawal( account , password ,withdrawal );
+                if( result2 ) {
+                    System.out.println("[안내] 출금이 완료되었습니다.");
+                }else{   // 메소드 값이 false면?
+                    System.out.println("[경고] 잔액이 부족합니다.");
+                }
+            } // if3 e
 
             if( choose == 4 ){
                 System.out.println("============== KB Bank  ==============");
                 System.out.println("--- 잔고 확인 ---");
-                System.out.println("계좌번호 :");
-                System.out.println("비밀번호 :");
-                System.out.println("[잔고] : %d \n ");
-            }
+                System.out.println("계좌번호 :");   String account = scan.next();
+                System.out.println("비밀번호 :");   int password = scan.nextInt();
+                int result3 = bc.getMoney( account , password );
+                if( result3 >= 0 ) {
+                    System.out.println("[잔고] : " + result3);
+                }
+            } // if4 e
+
         } // for e
     } // main e
 } // class e
