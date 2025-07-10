dc79e68e955292cdc540f79eefd2ca2fb3d0ba41
LEE-GYEO-RE <rufp9891@gmail.com>
2025-07-10 18:20:10 +0900

    자바 9일차 심화과제2 하는중~

--- src/심화/심화과제2/Account2.java
+++ src/심화/심화과제2/Account2.java
@@ -2,4 +2,16 @@ package 심화.심화과제2;
 
 public class Account2 {
 
+    String account; // 계좌번호 멤버변수
+    int password;   // 비밀번호 멤버변수
+    int money;      // 돈
+
+
+    // int balance;  잔액 멤버변수 -- > 굳이 필요없음 , 생성자에서 매개변수로 지정하고 , 인수값 받아오면 됨
+
+    Account2(String account , int password ){
+        this.account = account;
+        this.password = password;
+        this.money = 0;
+    }
 }
--- src/심화/심화과제2/BankService2.java
+++ src/심화/심화과제2/BankService2.java
@@ -1,5 +1,54 @@
 package 심화.심화과제2;
 
-public class BankService2 {
+import java.util.Scanner;
 
-}
+public class BankService2 {
+    public static void main(String[] args) {
+
+        for( ; ;){
+            System.out.println("=========== KB Bank ===========");
+            System.out.println("1.계좌등록 | 2.입금 | 3.출금 | 4.잔고 | 5.계좌이체");
+            System.out.println("============================");
+            System.out.println("선택 > ");
+
+            Scanner scan = new Scanner(System.in);
+            int choose = scan.nextInt();
+
+            if(choose == 1 ){
+                System.out.println("--- 계좌 등록 (보내는 사람) ---");
+                System.out.println("계좌번호 : ");  String account = scan.next();
+                System.out.println("비밀번호 : ");  int password = scan.nextInt();
+                System.out.println("[안내] 계좌 등록이 완료되었습니다.");
+            }
+
+            if(choose == 2 ){
+                System.out.println("--- 입금 ---");
+                System.out.println("계좌번호 : ");  String account = scan.next();
+                System.out.println("비밀번호 : ");  int password = scan.nextInt();
+                System.out.println("입금액 :");    int money = scan.nextInt();
+                System.out.println("[안내] 입금이 완료되었습니다.");
+            }
+
+            if(choose == 4 ){
+                System.out.println("--- 잔고 확인 (보내는 사람) ---");
+                System.out.println("계좌번호 : ");  String account = scan.next();
+                System.out.println("비밀번호 : ");  int password = scan.nextInt();
+                System.out.println("[잔고] :");    int money = scan.nextInt();
+            }
+
+            if(choose == 5 ){
+                System.out.println("--- 계좌 이체 ---");
+                System.out.println("계좌번호 : ");  String account = scan.next();
+                System.out.println("비밀번호 : ");  int password = scan.nextInt();
+                System.out.println("[잔고] :");    int money = scan.nextInt();
+            }
+
+
+
+
+
+
+        }
+
+    } // main e
+} // class e
