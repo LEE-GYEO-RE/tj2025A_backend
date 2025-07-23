a985de3554a7edfd37085da8f7e6fa9694a6ba80
LEE-GYEO-RE <rufp9891@gmail.com>
2025-07-24 01:23:39 +0900

    WaitingList DB연동해보기

--- src/day17/mydb0722.sql
+++ src/day17/mydb0722.sql
@@ -5,9 +5,9 @@ use mydb0722;
 create table waiting(
 	wno int auto_increment not null ,
     phone varchar(30) not null ,
-    member int not null,
+    count int not null,
     constraint primary key ( wno )
 );
 
 select * from waiting;
-insert into waiting ( phone , member ) values ('123' , 4 );
+insert into waiting ( phone , count ) values ('123' , 4 );
